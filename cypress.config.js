df3db3d8c29896de3afe6852ccd371234de23bff
Joonas Lindstedt <68861164+jslindgit@users.noreply.github.com>
2023-05-30 22:25:18 +0300

    11.9

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // eslint-disable-next-line no-console
+      console.log(on, config)
     },
   },
 })
